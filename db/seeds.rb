0bc20d2aea99424dd063dbd58237f8a170c6a85f
Drew Keat <thedrewkeat@gmail.com>
2021-08-21 12:55:43 -0600

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+10.times do
+    artist = Artist.new(name: Faker::Music::RockBand.name)
+    5.times do 
+        song = Song.new(title: Faker::Music::RockBand.song)
+        artist.songs << song
+    end
+    artist.save
+end
